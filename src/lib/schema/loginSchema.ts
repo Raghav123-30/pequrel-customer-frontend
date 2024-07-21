886705213e2c937a6d60c23707fcdbcd1d50ee4a
Raghav123-30 <raghavendrabhat2018@gmail.com>
2024-07-21 18:54:57 +0530

    Modified schema for password to allow special characters

--- src/lib/schema/loginSchema.ts
+++ src/lib/schema/loginSchema.ts
@@ -6,7 +6,7 @@ export const loginSchema = z.object({
 		.string()
 		.min(8, 'Password must be at least 8 characters long')
 		.regex(
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/,
 			'Password must contain at least one uppercase letter, one lowercase letter, and one number'
 		)
 		.refine((value) => {
--- src/lib/schema/resetAccountSchema.ts
+++ src/lib/schema/resetAccountSchema.ts
@@ -7,7 +7,7 @@ export const resetAccountSchema = z
 			.string()
 			.min(8, 'Password must be at least 8 characters long')
 			.regex(
-				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+				/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/,
 				'Password must contain at least one uppercase letter, one lowercase letter, and one number'
 			)
 			.max(20, 'Password must be no more than 20 characters long'), // Changed from refine to max for maximum length check
